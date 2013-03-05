fc74816f6b62f0c299e94f3bd4275e7199653390
Julie Santerre <umsanter@cc.umanitoba.ca>
2013-03-05 16:36:07 -0600

    Added mod_hours module

---
+++ mod_hours/mod_hours.php
@@ -0,0 +1,5 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+require JModuleHelper::getLayoutPath('mod_hours', $params->get('layout', 'default'));
+?>
---
+++ mod_hours/tmpl/default.php
@@ -0,0 +1,70 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+$sunday = $params->get('sunday');
+$monday = $params->get('monday');
+$tuesday = $params->get('tuesday');
+$wednesday = $params->get('wednesday');
+$thursday = $params->get('thursday');
+$friday = $params->get('friday');
+$saturday = $params->get('saturday');
+?>
+
+<style type="text/css">
+#hours-info {
+	padding-top: 20px;
+	padding-bottom: 20px;
+}
+#innerhours {
+	border-style:solid; 
+	border-width:10px;
+	border-color:rgba(255, 255, 255, 0.3);
+}
+#innerinnerhours {
+	background: #f2f2f2;
+	text-align:center !important;
+	font-size: 1em;
+	padding-top: 20px;
+	padding-bottom: 20px;
+	font-weight: bold;
+}
+</style>
+
+<div id="hours-info">
+	<div id="innerhours"><div id="innerinnerhours">
+	<div style="text-align:center;">
+		Hours of Operation
+	</div>
+	<table align='center'>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">SUN</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $sunday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">MON</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $monday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">TUE</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $tuesday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">WED</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $wednesday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">THU</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $thursday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">FRI</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $friday;?></td>
+		</tr>
+		<tr>
+			<td style="text-align:left; padding-right:30px; padding-top:10px;">SAT</td>
+			<td style="text-align:right; font-style:italic; padding-top:10px;"><?php echo $saturday;?></td>
+		</tr>
+	</table>
+	</div></div>
+</div>
+
